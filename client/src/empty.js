df782a8aff0a067c1ef7ab57660d4b860441bd0c
Jason-Francois <jasonc.francois@gmail.com>
2020-11-10 11:29:27 -0500

    Added navbar and blank message to planner page

---
+++ client/src/empty.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./empty.css";
+export default function Empty() {
+  return (
+    <div class="empty">
+      <h1>Your planner is empty!</h1>
+      <button class="btn" >Add a planner!</button>
+    </div>
+  );
+}
