91718690bf2c1c4a651df7e19064a21c79cbbb0b
Min Dye <mjd465@drexel.edu>
2020-11-18 20:22:19 -0500

    Added instructions and modified bash script

--- server/setup.sh
+++ server/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-psql -U postgres -f createdb.sql
-psql -U postgres -d myapp -f setup.sql
+echo Enter your postgres username
+read username
+
+psql -U $username -f createdb.sql
+psql -U $username -d myapp -f setup.sql
+
 echo Press any key to exit
 read
