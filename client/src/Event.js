91422451927d98e6cd38de1303e4cf617def8a0e
Jason-Francois <jasonc.francois@gmail.com>
2020-11-19 11:28:14 -0500

    Added a time input to the event form

--- client/src/Event.js
+++ client/src/Event.js
@@ -11,213 +11,25 @@ import {
 
 import "./event.css";
 
+//Function that handles Event component
 function Event() {
+  //Sets the original state of the form fields
   const [title, setTitle] = useState("");
   const [startMonth, setStartMonth] = useState("");
   const [startDay, setStartDay] = useState("");
   const [startYear, setStartYear] = useState("");
+  const [startTime, setStartTime] = useState("");
   const [endMonth, setEndMonth] = useState("");
   const [endDay, setEndDay] = useState("");
   const [endYear, setEndYear] = useState("");
+  const [endTime, setEndTime] = useState("");
+
   const [description, setDescription] = useState("");
+  //Sends a POST request to the server of all of the form data
   function add() {
-    alert("The form was sent");
-    console.log("startMonth");
+    console.log("Great!");
   }
   return (
-    // <>
-    //   <h1>Add Event</h1>
-    //   <form>
-    //     <div class="form-row">
-    //       <div class="labels form-group row col-sm-12">
-    //         <label for="title" class="col-sm-1 col-form-label">
-    //           Title
-    //         </label>
-    //         <div class="col-sm-12">
-    //           <input
-    //             type="text"
-    //             class="form-control"
-    //             id="title"
-    //             placeholder="Title"
-    //           />
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div class="form-row justify-content-between">
-    //       <div class="labels row col-sm-3">
-    //         <label for="start-time" class="col-sm-1 col-form-label">
-    //           Start Month
-    //         </label>
-    //         <div class="col-sm-12">
-    //           <select id="inputState" class="form-control">
-    //             <option selected>Choose...</option>
-    //             <option>01</option>
-    //             <option>02</option>
-    //             <option>03</option>
-    //             <option>04</option>
-    //             <option>05</option>
-    //             <option>06</option>
-    //             <option>07</option>
-    //             <option>08</option>
-    //             <option>09</option>
-    //             <option>10</option>
-    //             <option>11</option>
-    //             <option>12</option>
-    //           </select>
-    //         </div>
-    //       </div>
-    //       <div class="labels row col-sm-3">
-    //         <label for="start-time" class="col-sm-1 col-form-label">
-    //           Start Day
-    //         </label>
-    //         <div class="col-sm-12">
-    //           <select id="inputState" class="form-control">
-    //             <option selected>Choose...</option>
-    //             <option>01</option>
-    //             <option>02</option>
-    //             <option>03</option>
-    //             <option>04</option>
-    //             <option>05</option>
-    //             <option>06</option>
-    //             <option>07</option>
-    //             <option>08</option>
-    //             <option>09</option>
-    //             <option>10</option>
-    //             <option>11</option>
-    //             <option>12</option>
-    //             <option>13</option>
-    //             <option>14</option>
-    //             <option>15</option>
-    //             <option>16</option>
-    //             <option>17</option>
-    //             <option>18</option>
-    //             <option>19</option>
-    //             <option>20</option>
-    //             <option>21</option>
-    //             <option>22</option>
-    //             <option>23</option>
-    //             <option>24</option>
-    //             <option>25</option>
-    //             <option>26</option>
-    //             <option>27</option>
-    //             <option>28</option>
-    //             <option>29</option>
-    //             <option>30</option>
-    //             <option>31</option>
-    //           </select>
-    //         </div>
-    //       </div>
-    //       <div class="labels row col-sm-3">
-    //         <label for="start-time" class="col-sm-1 col-form-label">
-    //           Start Year
-    //         </label>
-    //         <div class="col-sm-12">
-    //           <input
-    //             type="text"
-    //             class="form-control"
-    //             id="start-year"
-    //             placeholder="Year"
-    //           />
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div class="form-row justify-content-between">
-    //       <div class="labels row col-sm-3">
-    //         <label for="start-time" class="col-sm-1 col-form-label">
-    //           End Month
-    //         </label>
-    //         <div class="col-sm-12">
-    //           <select id="inputState" class="form-control">
-    //             <option selected>Choose...</option>
-    //             <option>01</option>
-    //             <option>02</option>
-    //             <option>03</option>
-    //             <option>04</option>
-    //             <option>05</option>
-    //             <option>06</option>
-    //             <option>07</option>
-    //             <option>08</option>
-    //             <option>09</option>
-    //             <option>10</option>
-    //             <option>11</option>
-    //             <option>12</option>
-    //           </select>
-    //         </div>
-    //       </div>
-    //       <div class="labels row col-sm-3">
-    //         <label for="start-time" class="col-sm-1 col-form-label">
-    //           End Day
-    //         </label>
-    //         <div class="col-sm-12">
-    //           <select id="inputState" class="form-control">
-    //             <option selected>Choose...</option>
-    //             <option>01</option>
-    //             <option>02</option>
-    //             <option>03</option>
-    //             <option>04</option>
-    //             <option>05</option>
-    //             <option>06</option>
-    //             <option>07</option>
-    //             <option>08</option>
-    //             <option>09</option>
-    //             <option>10</option>
-    //             <option>11</option>
-    //             <option>12</option>
-    //             <option>13</option>
-    //             <option>14</option>
-    //             <option>15</option>
-    //             <option>16</option>
-    //             <option>17</option>
-    //             <option>18</option>
-    //             <option>19</option>
-    //             <option>20</option>
-    //             <option>21</option>
-    //             <option>22</option>
-    //             <option>23</option>
-    //             <option>24</option>
-    //             <option>25</option>
-    //             <option>26</option>
-    //             <option>27</option>
-    //             <option>28</option>
-    //             <option>29</option>
-    //             <option>30</option>
-    //             <option>31</option>
-    //           </select>
-    //         </div>
-    //       </div>
-    //       <div class="labels row col-sm-3">
-    //         <label for="start-time" class="col-sm-1 col-form-label">
-    //           End Year
-    //         </label>
-    //         <div class="col-sm-12">
-    //           <input
-    //             type="text"
-    //             class="form-control"
-    //             id="end-year"
-    //             placeholder="Year"
-    //           />
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div class="form-row">
-    //       <div class="labels row col-sm-12">
-    //         <label for="description" class=" labels col-sm-1 col-form-label">
-    //           Description
-    //         </label>
-    //         <div class="col-sm-12">
-    //           <p>
-    //             <input
-    //               class="form-action form-control"
-    //               type="text"
-    //               id="description"
-    //               placeholder="Description"
-    //             />
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </form>
-    // </>
     <>
       <div class="eventDiv">
         <div class="header text-center">
@@ -236,7 +48,7 @@ function Event() {
                 ></FormControl>
               </FormGroup>
             </Col>
-            <Col sm="4">
+            <Col sm="3">
               <FormGroup bsSize="large">
                 <ControlLabel>Start Month</ControlLabel>
                 <select
@@ -260,7 +72,7 @@ function Event() {
                 </select>
               </FormGroup>
             </Col>
-            <Col sm="4">
+            <Col sm="3">
               <FormGroup bsSize="large">
                 <ControlLabel>Start Day</ControlLabel>
                 <select
@@ -303,7 +115,7 @@ function Event() {
                 </select>
               </FormGroup>
             </Col>
-            <Col sm="4">
+            <Col sm="3">
               <FormGroup bsSize="large">
                 <ControlLabel>Start Year</ControlLabel>
                 <FormControl
@@ -314,7 +126,18 @@ function Event() {
                 ></FormControl>
               </FormGroup>
             </Col>
-            <Col sm="4">
+            <Col sm="3">
+              <FormGroup bsSize="large">
+                <ControlLabel>Start Time</ControlLabel>
+                <FormControl
+                  controlId="start-time"
+                  type="time"
+                  value={startTime}
+                  onChange={(e) => setStartTime(e.target.value)}
+                ></FormControl>
+              </FormGroup>
+            </Col>
+            <Col sm="3">
               <FormGroup bsSize="large">
                 <ControlLabel>End Month</ControlLabel>
                 <select
@@ -338,7 +161,7 @@ function Event() {
                 </select>
               </FormGroup>
             </Col>
-            <Col sm="4">
+            <Col sm="3">
               <FormGroup bsSize="large">
                 <ControlLabel>End Day</ControlLabel>
                 <select
@@ -381,7 +204,7 @@ function Event() {
                 </select>
               </FormGroup>
             </Col>
-            <Col sm="4">
+            <Col sm="3">
               <FormGroup bsSize="large">
                 <ControlLabel>End Year</ControlLabel>
                 <FormControl
@@ -389,6 +212,17 @@ function Event() {
                   type="text"
                   value={endYear}
                   onChange={(e) => setEndYear(e.target.value)}
+                ></FormControl>
+              </FormGroup>
+            </Col>
+            <Col sm="3">
+              <FormGroup bsSize="large">
+                <ControlLabel>End Time</ControlLabel>
+                <FormControl
+                  controlId="end-time"
+                  type="time"
+                  value={endTime}
+                  onChange={(e) => setEndTime(e.target.value)}
                 ></FormControl>
               </FormGroup>
             </Col>
@@ -405,7 +239,7 @@ function Event() {
             </Col>
           </Row>
 
-          <Button variant="secondary" type="submit">
+          <Button blockbSize="large" variant="secondary" type="submit">
             Add Event
           </Button>
         </Form>
