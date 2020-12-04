b9c9e665709457bb92ca871a428ab989d3e8a16c
Jason-Francois <jasonc.francois@gmail.com>
2020-12-03 22:07:56 -0500

    Added Log In/Out links for the navbar. Log out functionality still needed

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 
+//setting a variable for the style that can be applied to the objects
+const navStyle = {
+  color: "white",
+};
 
 function Nav() {
-    
-  //setting a variable for the style that can be applied to the objects
-  const navStyle = {
-    color: "white",
+  //Checks if the user is authenticated (Logged in)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (bool) => {
+    setIsAuthenticated(bool);
   };
+
+  const checkAuth = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/verify", {
+        method: "POST",
+        headers: { token: localStorage.token },
+      });
+
+      const parsedResponse = await response.json();
+
+      parsedResponse === true
+        ? setIsAuthenticated(true)
+        : setIsAuthenticated(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   return (
     //setting up navbar. adding the links that directs to the respective path
     // when the item is clicked on
@@ -17,13 +44,19 @@ function Nav() {
         <h3 id="title">UPlan</h3>
       </Link>
       <ul className="nav-links">
-        <Link style={navStyle} to="/login">
-          <li>Log in</li>
-        </Link>
+        {/* Change login link depending on authentication state */}
+        {isAuthenticated ? (
+          <Link style={navStyle} to="/login">
+            <li>Log Out</li>
+          </Link>
+        ) : (
+          <Link style={navStyle} to="/">
+            <li>Log In</li>
+          </Link>
+        )}
         <Link style={navStyle} to="/register">
           <li>Register</li>
         </Link>
-       
       </ul>
     </nav>
   );
