0b01458d9c63282a8f6791f9aa1eb31671636749
Jason-Francois <jasonc.francois@gmail.com>
2020-12-08 12:02:13 -0500

    Created the stylesheet planner.css for the component in Planner.js

--- client/src/components/pages/Planner.js
+++ client/src/components/pages/Planner.js
@@ -1,29 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Table } from "react-bootstrap";
-
-function renderTable(event) {
-  return (
-    <tr>
-      <td>{event.title}</td>
-      <td>{event.type}</td>
-      <td>{event.description}</td>
-      <td>{event.start_date.toDateString()}</td>
-      <td>{event.end_date.toDateString()}</td>
-    </tr>
-  );
-}
-//Converts ISO string dates to objects
-function formatDate(event) {
-  event.start_date = new Date(event.start_date.toString());
-  event.end_date = new Date(event.end_date.toString());
-}
+import "../styles/planner.css";
 //Planner component where table will be displayed
 function Planner() {
   //Set up state variables for use with the component
-
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const handleClick = (e) => {
+    console.log(`${e.target.value}`);
+  };
+  function renderTable(event) {
+    return (
+      <tr onClick={handleClick}>
+        <td>{event.title}</td>
+        <td>{event.type}</td>
+        <td>{event.description}</td>
+        <td>{event.start_date.toDateString()}</td>
+        <td>{event.end_date.toDateString()}</td>
+      </tr>
+    );
+  }
+  //Converts ISO string dates to objects
+  function formatDate(event) {
+    event.start_date = new Date(event.start_date.toString());
+    event.end_date = new Date(event.end_date.toString());
+  }
+
   //Set up GET request to get events for a user
   const getEvents = async () => {
     try {
@@ -45,12 +49,12 @@ function Planner() {
   }, []);
   return (
     <>
-      <h1>Planner Holder</h1>
+      <h1>Planner</h1>
       <Link to="/planner/event">
         <p>Add event</p>
       </Link>
-      <div id="tableContainer">
-        <Table>
+      <div id="table-div">
+        <Table >
           <thead>
             <th>Title</th>
             <th>Type</th>
