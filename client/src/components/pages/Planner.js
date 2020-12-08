2e2701634e38d7ad196b27940a1313c41f018b7a
Jason-Francois <jasonc.francois@gmail.com>
2020-12-07 23:55:55 -0500

    Refactored GET request in Planner.js; Began implementation of table

--- client/src/components/pages/Planner.js
+++ client/src/components/pages/Planner.js
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const getEvents = async () => {
-  try {
-    const response = await fetch("http://localhost:3001/planner", {
-      method: "GET",
-      headers: { token: localStorage.token },
-    });
-    const parseData = await response.json();
-    return parseData;
-  } catch (err) {
-    console.log(err.message);
-  }
-};
 
-getEvents().then((data) => console.log(data));
-
+//Planner component where table will be displayed
 function Planner() {
+  //Set up GET request to get events for a user
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const getEvents = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/planner", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+      const parseData = await response.json();
+      setUsers(parseData.events);
+      setLoading(false);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  useEffect(() => {
+    getEvents();
+  }, []);
   return (
     <>
       <h1>Planner Holder</h1>
       <Link to="/planner/event">
         <p>Add event</p>
       </Link>
+      <ul>{loading ? <p>Loading...</p> : <p>{users[1].title}</p>}</ul>
     </>
   );
 }
